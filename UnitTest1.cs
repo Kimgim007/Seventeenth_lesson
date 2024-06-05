dc7eceb967d9385ef5a600c42a2b947d2340b8d0
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-06-05 19:15:41 +0300

    Add Button and Click

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -18,7 +18,24 @@ namespace Seventeenth_lesson
         [Test]
         public void Test1()
         {
-           
+            Thread.Sleep(3000);
+            IWebElement buttonSeven = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[1]/div[5]/span"));
+            buttonSeven.Click();
+
+            Thread.Sleep(3000);
+            IWebElement buttonPlus = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[8]/span"));
+            buttonPlus.Click();
+
+            Thread.Sleep(3000);
+            IWebElement buttonFive = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[2]/div[6]/span"));
+            buttonFive.Click();
+
+            Thread.Sleep(3000);
+            IWebElement buttonEnter = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[10]/span"));
+            buttonEnter.Click();
+
+            Thread.Sleep(5000);
+            Assert.Pass();
         }
         [TearDown]
         public void TearDown()
