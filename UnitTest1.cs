a0d2742862ba5c9cdb8f5fcaaf65253d35bc695e
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-06-06 20:18:02 +0300

    Add WebDriverWait Delete Tread.Sleep Add Aseert.That().

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,40 +2,43 @@
 using NUnit.Framework.Internal.Execution;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace Seventeenth_lesson
 {
     public class Tests
     {
         IWebDriver driver;
+        WebDriverWait wait; 
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.desmos.com/scientific?lang=ru");
+            wait = new WebDriverWait(driver,TimeSpan.FromSeconds(10));
         }
 
         [Test]
         public void Test1()
         {
-            Thread.Sleep(3000);
-            IWebElement buttonSeven = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[1]/div[5]/span"));
+           
+            IWebElement buttonSeven = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[1]/div[5]/span")));
+            IWebElement buttonPlus = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[8]/span")));
+            IWebElement buttonFive = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[2]/div[6]/span")));   
+            IWebElement buttonEnter = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[10]/span")));
+
             buttonSeven.Click();
-
-            Thread.Sleep(3000);
-            IWebElement buttonPlus = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[8]/span"));
             buttonPlus.Click();
-
-            Thread.Sleep(3000);
-            IWebElement buttonFive = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[2]/div[6]/span"));
             buttonFive.Click();
-
-            Thread.Sleep(3000);
-            IWebElement buttonEnter = driver.FindElement(By.XPath("//*[@id='main']/div/div/div/div[3]/div[2]/div/div/div[4]/div[10]/span"));
             buttonEnter.Click();
 
+            IWebElement resultElement = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='main']/div/div/div/div[1]/div[2]/div/div[5]/div[2]/div[1]/span[2]")));
+            var resultText = resultElement.Text;
+            var result = resultText.Contains("12");
+
             Thread.Sleep(5000);
-            Assert.Pass();
+            Assert.That(result, Is.True);
         }
         [TearDown]
         public void TearDown()
